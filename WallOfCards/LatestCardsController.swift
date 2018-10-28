504cc879c136e2e38f3d23a1f612731910f14a67
Richard Price <mrspondooli@gmail.com>
2018-10-28 14:16:11 +0000

    created latestCardsVC with dummy cells

---
+++ WallOfCards/LatestCardsController.swift
@@ -0,0 +1,44 @@
+//
+//  LatestCardsController.swift
+//  WallOfCards
+//
+//  Created by Richard Price on 28/10/2018.
+//  Copyright © 2018 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class LatestCardsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: String(describing: UICollectionViewCell.self))
+        collectionView?.backgroundColor = .white
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: UICollectionViewCell.self), for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+
+}
