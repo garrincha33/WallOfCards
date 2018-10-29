022a04b2f85c1c8020b5b199a60b0b398f4a4397
Richard Price <mrspondooli@gmail.com>
2018-10-29 17:10:29 +0000

    using custom cell with moziavLayout

--- WallOfCards/LatestCardsController.swift
+++ WallOfCards/LatestCardsController.swift
@@ -19,39 +19,17 @@ class LatestCardsController: UICollectionViewController {
         self.collectionView?.collectionViewLayout = mosaicLayout
         mosaicLayout.delegate = (self as TRMosaicLayoutDelegate)
         //transparentNavBar()
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: String(describing: UICollectionViewCell.self))
+        collectionView?.register(LatestCardsCustomCell.self, forCellWithReuseIdentifier: String(describing: LatestCardsCustomCell.self))
         collectionView?.backgroundColor = UIColor.rgb(red: 26, green: 15, blue: 63)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
-    
-//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        return 1
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//
-//        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-//
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let width = (view.frame.width - 3 * 16) / 2
-//        return CGSize(width: width, height: width + 46)
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//
-//        return 16
-//
-//    }
-    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: UICollectionViewCell.self), for: indexPath)
+     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: LatestCardsCustomCell.self), for: indexPath) as! LatestCardsCustomCell
           let image = images[indexPath.row]
-          let imageView = UIImageView()
+          let imageView = cell.cardImageView
           imageView.image = image
           imageView.frame = cell.frame
           cell.backgroundView = imageView
--- WallOfCards/LatestCardsCustomCell.swift
+++ WallOfCards/LatestCardsCustomCell.swift
@@ -12,8 +12,7 @@ class LatestCardsCustomCell: UICollectionViewCell {
     
     let cardImageView: UIImageView = {
         let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.layer.cornerRadius = 8
+        iv.layer.cornerRadius = 16
         iv.clipsToBounds = true
         return iv
     }()
@@ -23,7 +22,6 @@ class LatestCardsCustomCell: UICollectionViewCell {
         super.init(frame: frame)
        // backgroundColor = .gray
         addSubview(cardImageView)
-        cardImageView.anchor(top: safeAreaLayoutGuide.topAnchor, left: safeAreaLayoutGuide.leftAnchor, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 20, paddingBottom: 20, paddingRight: 20, width: 150, height: 250)
     }
     
     required init?(coder aDecoder: NSCoder) {
